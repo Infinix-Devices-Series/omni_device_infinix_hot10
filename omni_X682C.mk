dea2df517749a09c9a09410346bcd80475d6367b
sarthakroy2002 <sarthakroy2002@gmail.com>
2021-07-06 08:59:46 +0530

    X682C: Build fastboot service

--- omni_X682C.mk
+++ omni_X682C.mk
@@ -34,6 +34,6 @@ PRODUCT_MANUFACTURER := infinix
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
